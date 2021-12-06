285ea900e855df04f6d3211cef332aabd2fe8219
Venki Ramachandran <venki_ramachandran@yahoo.com>
2021-12-06 13:20:37 +0530

    Add files via upload
    Original files

---
+++ TicTacToeGameEnvSetup.py
@@ -0,0 +1,121 @@
+# Submitted by : Venki Ramachandran
+# Dated: 30-OCt-2021
+# Game: TicTacToe with Numbers
+
+from gym import spaces
+import numpy as np
+import random
+from itertools import groupby
+from itertools import product
+
+
+
+class TicTacToe():
+
+    def __init__(self):
+        """initialising the board"""
+        # initialise state as an array
+        self.state = [np.nan for _ in range(9)]  # initialises the board position, can initialise to an array or matrix
+        # all possible numbers
+        self.all_possible_numbers = [i for i in range(1, len(self.state) + 1)] # , can initialise to an array or matrix
+        self.reset()
+
+
+    def is_winning(self, curr_state):
+        winning_sequences = [(0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6)]
+        
+        """Takes state as an input and returns whether any row, column or diagonal has winning sum
+        Example: Input state- [1, 2, 3, 4, nan, nan, nan, nan, nan]
+        Output = False"""
+        """ winning_pattern = [(0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6)]"""
+        
+        for pattern in winning_sequences:
+            if not np.isnan(curr_state[pattern[0]]) and not np.isnan(curr_state[pattern[1]]) and not np.isnan(curr_state[pattern[2]]):
+                pattern_state = curr_state[pattern[0]] + curr_state[pattern[1]] + curr_state[pattern[2]]
+                if pattern_state == 15:
+                    return True
+        return False
+
+
+    def is_terminal(self, curr_state):
+        # Terminal state could be winning state or when the board is filled up
+        if self.is_winning(curr_state) == True:
+            return True, 'Win'
+
+        elif len(self.allowed_positions(curr_state)) ==0:
+            return True, 'Tie'
+
+        else:
+            return False, 'Resume'
+
+
+
+    def allowed_positions(self, curr_state):
+        """Takes state as an input and returns all indexes that are blank"""
+        return [i for i, val in enumerate(curr_state) if np.isnan(val)]
+
+
+    def allowed_values(self, curr_state):
+        """Takes the current state as input and returns all possible (unused) values that can be placed on the board"""
+        used_values = [val for val in curr_state if not np.isnan(val)]
+        agent_values = [val for val in self.all_possible_numbers if val not in used_values and val % 2 !=0]
+        env_values = [val for val in self.all_possible_numbers if val not in used_values and val % 2 ==0]
+        return (agent_values, env_values)
+
+
+    def action_space(self, curr_state):
+        """Takes the current state as input and returns all possible actions, i.e, all combinations of allowed positions and allowed values"""
+
+        agent_actions = product(self.allowed_positions(curr_state), self.allowed_values(curr_state)[0])
+        env_actions = product(self.allowed_positions(curr_state), self.allowed_values(curr_state)[1])
+        return (agent_actions, env_actions)
+
+
+
+
+    def state_transition(self, curr_state, curr_action):
+        
+        """Takes current state and action and returns the board position just after agent's move.
+        Example: Input state- [1, 2, 3, 4, nan, nan, nan, nan, nan], action- [7, 9] or [position, value]
+        Output = [1, 2, 3, 4, nan, nan, nan, 9, nan]
+        """
+        
+        curr_state[curr_action[0]] = curr_action[1]
+        return curr_state
+
+
+    def step(self, curr_state, curr_action):
+        
+        
+        """Takes current state and action and returns the next state, reward and whether the state is terminal. Hint: First, check the board position after
+        agent's move, whether the game is won/loss/tied. Then incorporate environment's move and again check the board status.
+        Example: Input state- [1, 2, 3, 4, nan, nan, nan, nan, nan], action- [7, 9] or [position, value]
+        Output = ([1, 2, 3, 4, nan, nan, nan, 9, nan], -1, False)"""
+        return_state = False
+        n_state = self.state_transition(curr_state, curr_action)
+        return_state, game_status = self.is_terminal(n_state)
+        
+        if return_state == True:
+            if game_status == 'Win':
+                reward=10
+            else:
+                reward=0
+        else:
+            # DO the env step and check if it won
+            agent_actions, env_actions = self.action_space(n_state)
+            env_random_action = random.choice(list(env_actions))
+            n_state = self.state_transition(n_state, env_random_action)
+            terminal_status, result_status = self.is_terminal(n_state)
+        
+            # Check if Env won,
+            if return_state == True:
+                if game_status == 'Win':
+                    reward=-10 # Is true agent gets -1
+                else:
+                    reward=0 # IF tiw agent gets 0
+            else:
+                reward=-1 # agent gets -1 for every step by itself or by the env
+        return n_state, reward, return_state
+
+    def reset(self):
+        return self.state
